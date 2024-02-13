672750ed1e07a7c42e87962edfd3a79837274e89
iliana etaoin <iliana@oxide.computer>
2024-02-13 22:35:16 +0000

    crane 0.16.1

--- src/nix/flake.nix
+++ src/nix/flake.nix
@@ -7,7 +7,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    crane.url = "github:ipetkov/crane/v0.15.1";
+    crane.url = "github:ipetkov/crane/v0.16.1";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     nixie-tubes.url = "github:oxidecomputer/nixie-tubes";
   };
