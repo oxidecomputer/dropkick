c679c647734c75535e61a42194e393dcc53ef8d1
Ian Nickles <inickles@users.noreply.github.com>
2024-05-10 10:33:25 -0700

    move `output_path` arg from Build command to build::Args to bring the functionality to other build commands.

--- src/build.rs
+++ src/build.rs
@@ -9,6 +9,7 @@ use cargo_metadata::MetadataCommand;
 use clap::{Parser, ValueEnum};
 use serde::{Deserialize, Serialize};
 use std::io::{Read, Seek, SeekFrom, Write};
+use tempfile::NamedTempFile;
 
 #[derive(Debug, Parser, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -43,6 +44,10 @@ pub(crate) struct Args {
     #[clap(default_value = ".")]
     #[serde(skip_serializing)]
     pub(crate) package_dir: Utf8PathBuf,
+
+    /// Output path for built image (if not specified, the output is deleted)
+    #[clap(long)]
+    pub(crate) output_path: Option<Utf8PathBuf>,
 
     #[clap(flatten)]
     #[serde(flatten)]
@@ -143,19 +148,35 @@ impl Args {
         Ok(serde_json::to_string(&self.into_nixos_builder()?)?)
     }
 
-    pub(crate) fn create_iso(self, writer: &mut std::fs::File) -> Result<Metadata> {
+    pub(crate) fn create_iso(self) -> Result<(std::path::PathBuf, Metadata)> {
+        let output_path_arg = self.output_path.clone();
+        let (mut file, temp_path) = if let Some(output_path) = &output_path_arg {
+            NamedTempFile::new_in(output_path.parent().context("output path has no parent")?)?
+                .into_parts()
+        } else {
+            NamedTempFile::new()?.into_parts()
+        };
+
         let nixos_builder = self.into_nixos_builder()?;
-        let metadata = nixos_builder.build(writer)?;
+        let metadata = nixos_builder.build(&mut file)?;
 
         // append an empty ext4 filesystem to the image (see notes about /persist in config.nix)
         sparse_copy(
             &mut zstd::Decoder::new(include_bytes!("fs/ext4.zst").as_slice())?,
-            writer,
+            &mut file,
         )?;
-        let len = writer.stream_position()?;
-        writer.set_len(len)?;
+        let len = file.stream_position()?;
+        file.set_len(len)?;
 
-        Ok(metadata)
+        let output_path = if let Some(output_path) = output_path_arg {
+            temp_path.persist(&output_path)?;
+
+            output_path.into_std_path_buf()
+        } else {
+            temp_path.to_path_buf()
+        };
+
+        Ok((output_path, metadata))
     }
 }
 
